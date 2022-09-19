11d7c7db57740b0f27bbcaa42aa14a1f04357f5c
leduong12321 <duonglethanh2006@gmail.com>
2022-09-19 09:06:10 +0700

    add ui login

--- client/components/baocao/ChatLuongThanhPhamComponent.vue
+++ client/components/baocao/ChatLuongThanhPhamComponent.vue
@@ -1,150 +1,20 @@
 <template>
-    <div>
-      <div class="box">
-        <div class="d-flex align-items-center mb-2">
-          <div class="mr-2 text-date">Ngày bắt đầu:</div>
-          <date-picker
-            v-model="fromDay"
-            type="datetime"
-            placeholder="Ngày bắt đầu"
-            value-type="timestamp"
-            :show-time-panel="showTimePanel"
-            @close="handleOpenChange"
-            format="DD-MM-YYYY   HH:mm"
-            :class="checkError"
-            class="mr-4 date-ui"
-          >
-            <template v-slot:footer>
-              <button class="mx-btn mx-btn-text" @click="toggleTimePanel">
-                {{ showTimePanel ? "Chọn ngày" : "Chọn thời gian" }}
-              </button>
-            </template>
-          </date-picker>
-          <div class="mr-2 text-date">Ngày kết thúc:</div>
-          <date-picker
-            v-model="toDay"
-            type="datetime"
-            placeholder="Ngày kết thúc"
-            value-type="timestamp"
-            :show-time-panel="showTimePanel"
-            @close="handleOpenChange"
-            format="DD-MM-YYYY   HH:mm"
-            :class="checkError"
-            class="mr-3 date-ui"
-          >
-            <template v-slot:footer>
-              <button class="mx-btn mx-btn-text" @click="toggleTimePanel">
-                {{ showTimePanel ? "Chọn ngày" : "Chọn thời gian" }}
-              </button>
-            </template>
-          </date-picker>
-  
-          <b-button class="btn btn-submit submit ml-2" @click="handSubmit"
-            >Tìm kiếm</b-button
-          >
-        </div>
-        <p class="text-error-input-date" v-if="errorInputDate.length > 0">
-          {{ errorInputDate }}
-        </p>
-      </div>
-      <iframe
-        id="iframe"
-        v-if="API_URL"
-        :src="API_URL"
-        width="100%"
-        height="700px"
-        allowfullscreen
-        style="background-color: white"
-        class="mb-5 iframe"
-      ></iframe>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "baocaosanxuat-component",
+  <div>
+    <CommonReportComponent :API_URL="API_URL" :title="title"/>
+  </div>
+</template>
+<script>
+import CommonReportComponent from './CommonReportComponent.vue';
+export default {
+    name: "cahtluongthanhpham",
+    components: { CommonReportComponent },
     data() {
       return {
-        showTimePanel: false,
         API_URL: "https://localhost:44315/ChatLuongThanhPhamHRC",
-        fromDay: null,
-        toDay: null,
-        errorInputDate: "",
-      };
-    },
-    computed: {
-      checkError() {
-        if (this.fromDay > this.toDay) {
-          this.errorInputDate = "Có lỗi khi nhập thời gian";
-          return "error-input-date";
-        } else {
-          this.errorInputDate = "";
-          return "";
-        }
-      },
-    },
-    mounted() {
-      this.loadDataCurent();
-    },
-    methods: {
-      toggleTimePanel() {
-        this.showTimePanel = !this.showTimePanel;
-      },
-      handleOpenChange() {
-        this.showTimePanel = false;
-      },
-      loadDataCurent() {
-        this.fromDay = new Date(2021, 9, 24).setHours(0, 0, 0).valueOf();
-        this.toDay = new Date().setHours(23, 59, 59).valueOf();
-        this.API_URL =
-          "https://localhost:44315/ChatLuongThanhPhamHRC?from=" +
-          this.fromDay +
-          "&to=" +
-          this.toDay;
-      },
-      handSubmit() {
-        if (this.fromDay > this.toDay) {
-          return;
-        }
-        this.API_URL =
-          "https://localhost:44315/ChatLuongThanhPhamHRC?from=" +
-          this.fromDay +
-          "&to=" +
-          this.toDay;
-        document.getElementById("iframe").src =
-          document.getElementById("iframe").src;
-      },
-    },
-  };
-  </script>
-  
-  <style lang="scss">
-  .text-error-input-date {
-    color: #cc0033;
-    font-size: 13px;
-    padding-left: 120px;
-  }
-  .date-ui {
-    input {
-      border: 1px solid #555;
-    }
-    &.error-input-date {
-      input {
-        border: 1px solid #cc0033;
-        background-color: #fce4e4;
+        title: 'Báo cáo chất lượng thành phẩm HRC'
       }
     }
-  }
-  .text-date {
-    font-style: italic;
-  }
-  .submit {
-    color: #11101d;
-  }
-  .iframe {
-    border: 0px;
-    min-height: 800px;
-    max-height: calc(100% - 100px);
-  }
-  </style>
+}
+</script>
+
   
