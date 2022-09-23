4a5f33370d7c8c338c28d71f84650bbc20a0cc81
leduong12321 <duonglethanh2006@gmail.com>
2022-09-23 10:03:18 +0700

    handle login be

---
+++ server/ReportAPI/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using ReportAPI.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ReportAPI.Controllers
+{
+    [EnableCors(origins: "https://localhost:44315", headers: "*", methods: "*")]
+    public class UserController : ApiController
+    {
+        User[] user = new User[]
+        {
+            new User { Id = 1, Name = "Admin", UserName = "admin", Password = "admin", Description = "Full access control", Role = 0 },
+            new User { Id = 2, Name = "KTV", UserName = "ktv", Password = "ktv", Description = "Limited access", Role = 1 },
+        };
+
+        public dynamic Get()
+        {
+            return user;
+        }
+        [HttpPost]
+        public dynamic Post([FromBody] Dictionary<string, string> value)
+        { 
+            try
+            {
+                dynamic people = null;
+                foreach (dynamic item in user)
+                {
+                    if (item.UserName == value["Username"] && item.Password == value["Password"])
+                    {
+                        people = item;
+                    }
+                }
+                return people;
+            }
+            catch (Exception)
+            {
+                return "error";
+            }
+        }
+    }
+}
