9c24deda8834dd83e851caf7ec675febbe8f8324
leduong12321 <duonglethanh2006@gmail.com>
2022-09-30 13:05:22 +0700

    add ui report bur & wr

---
+++ client/pages/baocao/thay-truc-wr.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <ThayTrucWr />
+    </div>
+  
+  </template>
+  
+  <script>
+import ThayTrucWr from '../../components/baocao/ThayTrucWr.vue';
+  
+  export default {
+    name: "homepage",
+    middleware: "auth",
+    components: { ThayTrucWr }
+}
+  </script>
